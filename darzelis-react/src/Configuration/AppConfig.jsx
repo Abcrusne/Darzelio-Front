f97d599b26f280ea0667a9f3836d8465aeb30534
Abcrusne <rusne.pet@gmail.com>
2021-01-29 15:50:16 +0200

    app config

--- darzelis-react/src/Configuration/AppConfig.jsx
+++ darzelis-react/src/Configuration/AppConfig.jsx
@@ -1,3 +1,3 @@
-export const API = 'http://akademijait.vtmc.lt:8181';
+export const API = 'http://akademijait.vtmc.lt:8181/bean-app';
 // export const API = '/bean-app';
 // export const API = process.env.PUBLIC_URL;
