0ff14729228ee58187f84f6e9c5194923c1a5426
Abcrusne <rusne.pet@gmail.com>
2021-01-29 14:09:58 +0200

    App config ir package.json homepage pakeistas

---
+++ darzelis-react/src/Components/SysAdminLanding/NavigationComponent.jsx
@@ -0,0 +1,49 @@
+import { NavLink } from 'react-router-dom';
+import React from 'react';
+
+function NavigationComponent(props) {
+  return (
+    <div>
+      {/* <nav className=" nav flex-column navbar-light bg-light"> */}
+        <div className="container">
+          <ul className="nav flex-column">
+            <li className="nav-item disabled">
+              <NavLink to="/admin/pradzia" className="nav-link disabled">
+              <span class="navbar-brand mb-0 h1">Sistemos administratorius</span>
+              </NavLink>
+            </li>
+            <li className="nav-item disabled">
+              <NavLink to="/admin/pradzia" className="nav-link ">
+                Pradžia
+              </NavLink>
+            </li>
+           
+            <li className="nav-item active">
+              <NavLink to="/admin/vartotojai" className="nav-link">
+                Vartotojų sąrašas
+              </NavLink>
+            </li>
+            <li className="nav-item active">
+              <NavLink to="/admin/prasymai" className="nav-link">
+                Prašymai
+              </NavLink>
+            </li>
+            <li className="nav-item active">
+              <NavLink to="/admin/eiliutvarkymas" className="nav-link">
+                Eilių tvarkymas
+              </NavLink>
+            </li>
+            <li className="nav-item active">
+              <NavLink to="/" className="nav-link">
+               Atsijungti
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      {/* </nav> */}
+      {props.children}
+    </div>
+  );
+}
+
+export default NavigationComponent;
---
+++ darzelis-react/src/Components/SysAdminLanding/SysAdminLanding.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import NavigationComponent from './NavigationComponent'
+
+export default class SysAdminLanding extends Component {
+    render() {
+        return (
+            <div>
+                <NavigationComponent/>
+            </div>
+        )
+    }
+}
