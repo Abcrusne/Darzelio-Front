701042648638d41d0e00fef70bc6a8c99f433b93
agniokas <agniokas@gmail.com>
2021-01-29 11:12:29 +0200

    Authentication and authirisation not functioning right yet

--- darzelis-react/src/App.js
+++ darzelis-react/src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Switch, Route } from 'react-router';
+import {Switch, Route} from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
-import RegistrationFormContainer from "./Components/Registration/RegistrationFormContainer"
-import LoginFormContainer from "./Components/Login/LoginFormContainer";
-import LandingPage from "./Components/LandingPage"
 
-function App() {
+
+import RegistrationFormContainer from "./Components/Registration/RegistrationFormContainer"
+import LandingPage from "./Components/LandingPage"
+import LoginSuccess from "./Components/Login/LoginSuccess";
+import PrivateRoute from "./Configuration/PrivateRoute";
+
+
+const App = () => {
+
   return (
     <div className="App">
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
-          <Route exact path="/registracija" component={RegistrationFormContainer}/>
           <Route exact path="/" component={LandingPage} />
+          <Route exact path="/registracija" component={RegistrationFormContainer} />
+          <Route path="/login" component={LandingPage} />
+          <PrivateRoute exact path="/dashboard" component={LoginSuccess} roles={"PARENT"} />
+          {/*<PrivateRoute exact path="/admin" component={AdminDashboard} />*/}
+          {/*<PrivateRoute exact path="/admin/edu" component={EduDashboard} />*/}
         </Switch>
       </BrowserRouter>
     </div>
