b4fcb30bbad930af256a598b5f223bdfada6754a
agniokas <57033965+agniokas@users.noreply.github.com>
2021-02-02 14:53:57 +0200

    BEAN-96-44 veikiantis

--- darzelis-react/src/index.js
+++ darzelis-react/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+document.title = "Vaikų darželių informacinė sistema";
 ReactDOM.render(
   <React.StrictMode>
      <App />
