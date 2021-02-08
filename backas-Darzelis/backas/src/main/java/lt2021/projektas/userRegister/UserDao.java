16f22d092100a6c6a5b88629bbf2fe8f830fc5b8
agniokas <57033965+agniokas@users.noreply.github.com>
2021-02-08 09:39:39 +0200

    not needed

--- backas-Darzelis/backas/src/main/java/lt2021/projektas/userRegister/UserDao.java
+++
@@ -1,9 +0,0 @@
-package lt2021.projektas.userRegister;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Long> {
-	
-	User findByEmail(String email);
-	
-}
