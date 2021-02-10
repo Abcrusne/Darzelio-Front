bcadebd2a23bdc70d1014b27bbc3028e56685380
agniokas <57033965+agniokas@users.noreply.github.com>
2021-02-10 11:56:58 +0200

    Delete backas directory

--- backas/src/main/java/lt2021/projektas/parentdetails/ParentDetailsDao.java
+++
@@ -1,9 +0,0 @@
-package lt2021.projektas.parentdetails;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParentDetailsDao extends JpaRepository<ParentDetails, Long> {
-	
-	
-	
-}
