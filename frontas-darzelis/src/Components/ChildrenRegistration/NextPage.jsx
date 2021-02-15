b43e115540f15da46e4832ec2ada047eb82c4f0a
Abcrusne <rusne.pet@gmail.com>
2021-02-15 11:22:18 +0200

    validacija adresams, gimimo datai

--- frontas-darzelis/src/Components/ChildrenRegistration/NextPage.jsx
+++ frontas-darzelis/src/Components/ChildrenRegistration/NextPage.jsx
@@ -1,41 +1,36 @@
-import React from 'react'
+import React from 'react';
 import { useHistory } from 'react-router';
 
+const NextPage = () => {
+  const history = useHistory();
+  const addAnotherChild = (e) => {
+    e.preventDefault();
+    history.push('/tevai/vaikoregistracija');
+  };
+  const registerToKindergarten = (e) => {
+    e.preventDefault();
+    history.push('/tevai/registracija-i-darzeli');
+  };
 
+  return (
+    <div className="mt-5 mr-5 ml-5">
+      <button
+        type="submit"
+        className="btn btn-success btn-lg btn-block"
+        onClick={addAnotherChild}
+      >
+        Pridėti kitą vaiką
+      </button>
 
-const NextPage=()=> {
-    const history = useHistory();
-    const addAnotherChild =(e)=>{
-        e.preventDefault();
-        history.push('/tevai/vaikoregistracija');
-    }
-    const registerToKindergarten =(e)=>{
-        e.preventDefault();
-        history.push('/tevai/dashboard');
-    }
-
-    return (
-        <div className="mt-5 mr-5 ml-5">
-           
-                <button
-                type="submit"
-                className="btn btn-success btn-lg btn-block"
-               onClick={addAnotherChild}
-              >
-                Pridėti kitą vaiką
-              </button>
-          
-           
-              <button
-                type="submit"
-                className="btn btn-success btn-lg btn-block "
-               onClick={registerToKindergarten}
-              >
-               Tęsti
-              </button>
-    
-        </div>
-    )
-}
+      <button
+        type="submit"
+        className="btn btn-success btn-lg btn-block "
+        onClick={registerToKindergarten}
+      >
+        Tęsti
+      </button>
+    </div>
+  );
+};
 
 export default NextPage;
