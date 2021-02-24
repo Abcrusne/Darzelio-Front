d599498cdddf7b167ae2ad8a66d6a75bbefff014
agniokas <agniokas@gmail.com>
2021-02-24 13:22:59 +0200

    BEAN-176 List of registrations to kindergarden with start and end buttons

--- frontas-darzelis/src/Components/MainRegistration/ListOfRegistrations.jsx
+++ frontas-darzelis/src/Components/MainRegistration/ListOfRegistrations.jsx
@@ -1,10 +1,87 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from "axios";
 
-const ListOfRegistrations = () => {
-    return(
-        <div>
-            <h1>ListOfRegistrations</h1>
-        </div>
-    )
+//our imports
+import {API} from "../../Configuration/AppConfig";
+import ListOfRegistrationsPresentation from "./ListOfRegistrationsPresentation";
+import "../../Style/ParentLanding.css"
+
+class ListOfRegistrations extends Component {
+    constructor() {
+        super();
+        this.state = {
+            admissionQues: [],
+        };
+    }
+    componentDidMount = () => {
+        axios
+            .get(`${API}/api/kindergartens/admissions`)
+            .then((response) => {
+                this.setState({ admissionQues: response.data });
+                }
+            )
+            .catch((error) => console.log(error))
+
+    };
+
+   handleStartClick = () => {
+        axios
+            .post(`${API}/api/kindergartens/startadmission`)
+            .then(response => {
+                console.log(response.data);
+                alert('Sukurta nauja registracijų į darželius eilė, kurios pradžia ' + response.data)
+            })
+            .catch(error => alert('Negalima atidaryti naujos registracijų eilės, kol yra aktyvi registracija! Pirma sustabdykite aktyvią eilę, tuomet atidarykite naują.'))
+    }
+
+
+
+    render (){
+        const Ques = this.state.admissionQues;
+        return(
+            <div>
+                <div className="container mt-5">
+                    <div className="mb-4">
+                        <h4>Registracijų į darželius sąrašas</h4>
+                    </div>
+                    <table className="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Pradžios data</th>
+                            <th scope="col">Pabaigos data</th>
+                            <th scope="col">Atidarytas (taip/ne)</th>
+                            <th scope="col">Viso registracijų</th>
+                            <th scope="col">Viso vietų darželiuose</th>
+                            <th scope="col">Ar visos eilės patvirtintos</th>
+                            <th scope="col">Peržiūrėti sąrašus</th>
+                            <th scope="col"></th>
+                            <th scope="col"></th>
+                        </tr>
+                        </thead>
+
+                        {this.state.admissionQues.length > 0 ? (
+                            <tbody>
+                            <ListOfRegistrationsPresentation
+                                admissionQues={this.state.admissionQues}
+                            />
+                            </tbody>
+                        ) : <h5>...</h5>}
+                        <tfoot>
+                            <td colSpan="8">
+                                <button
+                                    type="button"
+                                    className="mr-4 btn text-nowrap"
+                                    onClick={this.handleStartClick}
+                                >Atidaryti registraciją į darželius
+                                </button>
+                            </td>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+
 }
 export default ListOfRegistrations;
---
+++ frontas-darzelis/src/Components/MainRegistration/ListOfRegistrationsPresentation.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+//our imports
+import {API} from "../../Configuration/AppConfig";
+import "../../Style/ParentLanding.css"
+import {ModalForStopButton} from "../Modal/ModalForStopButton";
+
+const ListOfRegistrationsPresentation = ({admissionQues, onClick}) => {
+
+    const handleEndClick = () => {
+        axios
+            .post(`${API}/api/kindergartens/stopadmission`)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => console.log(error))
+    }
+
+    return (
+            admissionQues.map(
+            ({ id, active, allQuesConfirmed, startDate, endDate, registrationCount, totalSpotsInKindergartens },
+            index) => {
+            return (
+            <tr key={id}>
+            <th scope="row">{index + 1}</th>
+            {/*<td>{id}</td>*/}
+            <td>{startDate}</td>
+            <td>{endDate}</td>
+            {active ? <td>Taip</td> : <td>Ne</td>}
+            <td>{registrationCount}</td>
+            <td>{totalSpotsInKindergartens}</td>
+            {allQuesConfirmed ? <td>Taip</td> : <td>Ne</td>}
+            <td>
+                <Link
+                    className="btn btn-link-1"
+                    to={`${API}/admin/edu/priemimai/${id}/eiles`}
+                >
+                    Į sąrašą
+                </Link>
+            </td>
+            <td>
+                {active ? (<div>
+                    <button
+                    className="mr-4 btn btn-danger"
+                    data-toggle="modal"
+                    data-target={`#staticBackdrop${id}`}
+                    value={id}
+                    >Stabdyti
+                    </button>
+                    </div>)
+                 : null}
+            </td>
+            <td>
+                <ModalForStopButton
+                    QueId={id}
+                    onClick={handleEndClick}
+                />
+            </td>
+            </tr>
+            );
+        })
+    )
+}
+
+export default ListOfRegistrationsPresentation
