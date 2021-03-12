85bd718e5b7a4bda4a58af31d775e6a73719319d
Abcrusne <rusne.pet@gmail.com>
2021-03-12 12:32:30 +0200

    naudojimosi instrukcija pdf

---
+++ frontas-darzelis/src/Components/Guide/ParentsGuide.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import pdf from '../Guide/Tevu.pdf';
+import '../../Style/style.css';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+const Guide = () => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  //   const onDocumentLoadSuccess =({ numPages })  =>{
+  //     setNumPages(numPages);
+  //   };
+  return (
+    <div className="pdfGuide">
+      <Document
+        file={pdf}
+        onLoadSuccess={({ numPages }) => {
+          setNumPages(numPages);
+        }}
+        loading="..."
+        onLoadError={(error) => alert('Nepavyko! ')}
+        error="Nepavyko pakrauti PDF failo!"
+        noData="Nėra duomenų!"
+      >
+        {/* <Page pageNumber={pageNumber} /> */}
+        {Array.apply(null, Array(numPages))
+          .map((x, i) => i + 1)
+          .map((page) => (
+            <Page pageNumber={page} />
+          ))}
+      </Document>
+      <p>Iš viso puslapių: {numPages}</p>
+      {/* <p>
+        Puslapis {pageNumber} iš {numPages}
+      </p> */}
+    </div>
+  );
+};
+export default Guide;
