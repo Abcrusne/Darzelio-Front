6f7f188c61c383cbbfd92b7b25a9d0295a0ecf47
Abcrusne <rusne.pet@gmail.com>
2021-03-15 10:59:18 +0200

    tevas+edu istrina savo duomenis ir arba paskyra

--- frontas-darzelis/src/Components/EduAdminLanding/EduAdminRoutes.jsx
+++ frontas-darzelis/src/Components/EduAdminLanding/EduAdminRoutes.jsx
@@ -17,55 +17,54 @@ import UpdateUserPasswordContainer from '../UserData/UpdateUserPasswordContainer
 import KindergartenTableContainer from '../Queue/KindergartenTableContainer';
 import EduAdminChildInfoContainer from './EduAdminChildInfoContainer';
 import EduAdminUpdateChildApplicationContainer from './EduAdminUpdateChildApplicationContainer';
-import RegisteredChildrenQueueList from "../ChildrenRegistrationQue/RegisteredChildrenQueueList";
+import RegisteredChildrenQueueList from '../ChildrenRegistrationQue/RegisteredChildrenQueueList';
 import UpdateUserEmailContainer from '../UserData/UpdateUserEmailContainer';
 import HorizontalChart from '../Statistics/HorizontalChart';
 import EduGuide from '../Guide/EduGuide';
-
-
+import DeleteData from '../UserData/DeleteData';
 
 const EduAdminRoutes = () => {
-    return (
-        <Switch>
-            <PrivateRoute
-                path="/admin/edu"
-                exact
-                component={EduAdminDashboard}
-                role={'EDU'}
-                />
-   
-            <PrivateRoute
-                path="/admin/edu/vaikai"
-                exact
-                component={RegisteredChildrenQueueList}
-                role={'EDU'}
-            />
-            <PrivateRoute
-                path="/admin/edu/registracijos"
-                exact
-                component={ListOfRegistrations}
-                role={'EDU'}
-            />
+  return (
+    <Switch>
+      <PrivateRoute
+        path="/admin/edu"
+        exact
+        component={EduAdminDashboard}
+        role={'EDU'}
+      />
 
-            <PrivateRoute
-                path="/admin/edu/darzeliai"
-                exact
-                component={KindergartenListTableContainer}
-                role={'EDU'}
-            />
-           <PrivateRoute
-                path="/admin/edu/darzelioregistracija"
-                exact
-                component={KindergartenRegistrationContainer}
-                role={'EDU'}
-            />
-            <PrivateRoute
-                path="/admin/edu/darzeliai/:id"
-                exact
-                component={UpdateKindergartenFormContainer}
-                role={'EDU'}
-            />
-            <PrivateRoute
+      <PrivateRoute
+        path="/admin/edu/vaikai"
+        exact
+        component={RegisteredChildrenQueueList}
+        role={'EDU'}
+      />
+      <PrivateRoute
+        path="/admin/edu/registracijos"
+        exact
+        component={ListOfRegistrations}
+        role={'EDU'}
+      />
+
+      <PrivateRoute
+        path="/admin/edu/darzeliai"
+        exact
+        component={KindergartenListTableContainer}
+        role={'EDU'}
+      />
+      <PrivateRoute
+        path="/admin/edu/darzelioregistracija"
+        exact
+        component={KindergartenRegistrationContainer}
+        role={'EDU'}
+      />
+      <PrivateRoute
+        path="/admin/edu/darzeliai/:id"
+        exact
+        component={UpdateKindergartenFormContainer}
+        role={'EDU'}
+      />
+      <PrivateRoute
         path="/admin/edu/naudotojo-duomenys"
         exact
         component={UserData}
@@ -91,34 +90,40 @@ const EduAdminRoutes = () => {
         role={'EDU'}
       />
       <PrivateRoute
-                path="/admin/edu/vaikai/:id"
-                exact
-                component={EduAdminChildInfoContainer}
-                role={'EDU'}
-            /> 
-         
-            {/* <PrivateRoute
+        path="/admin/edu/vaikai/:id"
+        exact
+        component={EduAdminChildInfoContainer}
+        role={'EDU'}
+      />
+
+      {/* <PrivateRoute
                 path="/admin/edu/vaikai/:id/redaguoti"
                 exact
                 component={EduAdminUpdateChildApplicationContainer}
                 role={'EDU'}
             />  */}
-               <PrivateRoute
+      <PrivateRoute
         path="/admin/edu/duomenys/redaguoti/pasta"
         exact
         component={UpdateUserEmailContainer}
         role={'EDU'}
       />
-                <PrivateRoute
+      <PrivateRoute
         path="/admin/edu/statistika"
         exact
         component={HorizontalChart}
         role={'EDU'}
       />
-                 <PrivateRoute
+      <PrivateRoute
         path="/admin/edu/instrukcija"
         exact
         component={EduGuide}
+        role={'EDU'}
+      />
+      <PrivateRoute
+        path="/admin/edu/duomenys/istrinti"
+        exact
+        component={DeleteData}
         role={'EDU'}
       />
       <PrivateRoute
@@ -127,7 +132,6 @@ const EduAdminRoutes = () => {
         component={NotFoundPage}
         role={'EDU'}
       />
-
     </Switch>
   );
 };
